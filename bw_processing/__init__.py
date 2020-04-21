f6ef222f42431ce9ecfb29b6c0e92854c41e4470
Chris Mutel <cmutel@gmail.com>
2020-04-21 15:54:24 +0200

    Fix init imports

--- bw_processing/__init__.py
+++ bw_processing/__init__.py
@@ -2,19 +2,19 @@ from .version import version as __version__
 
 from .calculation_package import format_calculation_resource, create_calculation_package
 from .utils import (
-    MAX_SIGNED_32BIT_INT,
-    COMMON_DTYPE,
-    NAME_RE,
     chunked,
-    dictionary_formatter,
-    create_numpy_structured_array,
+    COMMON_DTYPE,
     create_datapackage_metadata,
+    create_numpy_structured_array,
+    dictionary_formatter,
+    MAX_SIGNED_32BIT_INT,
+    NAME_RE,
 )
 from .processed_package import (
-    create_processed_datapackage,
-    greedy_set_cover,
     as_unique_attributes,
+    create_processed_datapackage,
     format_processed_resource,
+    greedy_set_cover,
 )
 from .loading import load_package
 from .filesystem import safe_filename, clean_datapackage_name
