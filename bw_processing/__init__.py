ae00b99a26fddab611ee33eb805c26bcc767d6f5
Chris Mutel <cmutel@gmail.com>
2020-09-25 15:41:03 +0200

    Cleanup init

--- bw_processing/__init__.py
+++ bw_processing/__init__.py
@@ -1,15 +1,27 @@
+__all__ = (
+    "__version__",
+    "as_unique_attributes",
+    "as_unique_attributes_dataframe",
+    "COMMON_DTYPE",
+    "create_array",
+    "create_structured_array",
+    "Datapackage",
+    "DEFAULT_LICENSES",
+    "dictionary_wrapper",
+    "GenericProxy",
+    "indices_wrapper",
+    "MAX_SIGNED_32BIT_INT",
+    "md5",
+    "ReadProxy",
+    "safe_filename",
+)
+
 from .version import version as __version__
 
 from .datapackage import Datapackage
 from .constants import COMMON_DTYPE, DEFAULT_LICENSES, MAX_SIGNED_32BIT_INT
-from .utils import chunked, dictionary_formatter, dictionary_wrapper, indices_wrapper
+from .utils import dictionary_wrapper, indices_wrapper
 from .filesystem import md5, safe_filename
-
-# from .processed_package import (
-#     as_unique_attributes,
-#     create_processed_datapackage,
-#     format_processed_resource,
-#     greedy_set_cover,
-# )
-from .filesystem import safe_filename, clean_datapackage_name
 from .unique_fields import as_unique_attributes_dataframe, as_unique_attributes
+from .array_creation import create_array, create_structured_array
+from .proxies import ReadProxy, GenericProxy
