c1113d6bc40003d6d7fef0e17ccc7b229a5cf0cd
Chris Mutel <cmutel@gmail.com>
2021-10-30 10:45:49 +0200

    Add tests for checking array shapes and dtypes

--- tests/datapackage.py
+++ tests/datapackage.py
@@ -8,8 +8,13 @@ from fs.memoryfs import MemoryFS
 from fs.osfs import OSFS
 
 from bw_processing import create_datapackage, load_datapackage
-from bw_processing.constants import INDICES_DTYPE
-from bw_processing.errors import NonUnique, PotentialInconsistency, WrongDatatype
+from bw_processing.constants import INDICES_DTYPE, UNCERTAINTY_DTYPE
+from bw_processing.errors import (
+    NonUnique,
+    PotentialInconsistency,
+    ShapeMismatch,
+    WrongDatatype,
+)
 
 
 class Dummy:
@@ -181,63 +186,188 @@ def test_del_resource_group_error_modifications(tmp_path):
         dp.del_resource_group("sa-vector-interface")
 
 
-def test_wrong_dtype_flip_array():
-    pass
-
-
-def test_add_vector_as_array():
-    pass
-
-
-def test_add_array_as_vector():
-    return
-    data_array = np.array([(2, 7, 12)])
+def test_add_persistent_vector_data_shapemismatch_ndimensions():
+    dp = create_datapackage()
+    data_array = np.array([[2, 7, 12], [4, 5, 15]])
     indices_array = np.array([(1, 4), (2, 5), (3, 6)], dtype=INDICES_DTYPE)
-    flip_array = np.array([1, 0, 0], dtype=bool)
-    dp.add_persistent_vector(
-        matrix="sa_matrix",
-        data_array=data_array,
-        name="sa-data-vector",
-        indices_array=indices_array,
-        nrows=2,
-        flip_array=flip_array,
-    )
+    with pytest.raises(ShapeMismatch):
+        dp.add_persistent_vector(
+            matrix="sa_matrix",
+            data_array=data_array,
+            name="sa-data-vector",
+            indices_array=indices_array,
+        )
 
-    json_data = [{"a": "b"}, 1, True]
-    df = pd.DataFrame(
+
+def test_add_persistent_vector_data_shapemismatch_nrows():
+    dp = create_datapackage()
+    data_array = np.array([2, 7, 12, 15])
+    indices_array = np.array([(1, 4), (2, 5), (3, 6)], dtype=INDICES_DTYPE)
+    with pytest.raises(ShapeMismatch):
+        dp.add_persistent_vector(
+            matrix="sa_matrix",
+            data_array=data_array,
+            name="sa-data-vector",
+            indices_array=indices_array,
+        )
+
+
+def test_add_persistent_vector_distributions_shapemismatch():
+    dp = create_datapackage()
+    distributions_array = np.array(
         [
-            {"id": 1, "a": 1, "c": 3, "d": 11},
-            {"id": 2, "a": 2, "c": 4, "d": 11},
-            {"id": 3, "a": 1, "c": 4, "d": 11},
-        ]
-    ).set_index(["id"])
+            (3, 1.3, 2.5, np.NaN, np.NaN, np.NaN, False),
+            (0, 1.3, 2.5, np.NaN, np.NaN, np.NaN, False),
+        ],
+        dtype=UNCERTAINTY_DTYPE,
+    )
+    indices_array = np.array([(1, 4), (2, 5), (3, 6)], dtype=INDICES_DTYPE)
+    with pytest.raises(ShapeMismatch):
+        dp.add_persistent_vector(
+            matrix="sa_matrix",
+            distributions_array=distributions_array,
+            name="sa-data-vector",
+            indices_array=indices_array,
+        )
 
-    dp.add_persistent_array(
-        matrix="sa_matrix",
-        data_array=np.arange(12).reshape((3, 4)),
-        indices_array=indices_array,
-        name="sa-data-array",
-        flip_array=flip_array,
-    )
 
-    dp.add_dynamic_vector(
-        interface=Dummy(),
-        indices_array=indices_array,
-        matrix="sa_matrix",
-        name="sa-vector-interface",
-    )
+def test_add_persistent_vector_flip_dtype():
+    dp = create_datapackage()
+    data_array = np.array([2, 7, 12])
+    flip_array = np.array([0, 1, 0])
+    indices_array = np.array([(1, 4), (2, 5), (3, 6)], dtype=INDICES_DTYPE)
+    with pytest.raises(WrongDatatype):
+        dp.add_persistent_vector(
+            matrix="sa_matrix",
+            data_array=data_array,
+            name="sa-data-vector",
+            flip_array=flip_array,
+            indices_array=indices_array,
+        )
 
-    dp.add_dynamic_array(
-        interface=Dummy(),
-        matrix="sa_matrix",
-        name="sa-array-interface",
-        indices_array=indices_array,
-    )
-    dp.add_csv_metadata(
-        dataframe=df,
-        valid_for=[("sa-data-vector", "rows")],
-        name="sa-data-vector-csv-metadata",
-    )
-    dp.add_json_metadata(
-        data=json_data, valid_for="sa-data-array", name="sa-data-array-json-metadata"
-    )
+
+def test_add_persistent_vector_flip_shapemistmatch():
+    dp = create_datapackage()
+    data_array = np.array([2, 7, 12])
+    flip_array = np.array([0, 1, 0, 1], dtype=bool)
+    indices_array = np.array([(1, 4), (2, 5), (3, 6)], dtype=ShapeMismatch)
+    with pytest.raises(ShapeMismatch):
+        dp.add_persistent_vector(
+            matrix="sa_matrix",
+            data_array=data_array,
+            name="sa-data-vector",
+            flip_array=flip_array,
+            indices_array=indices_array,
+        )
+
+
+def test_add_persistent_array_data_shapemismatch_ndimensions():
+    dp = create_datapackage()
+    data_array = np.array([2, 7, 12])
+    indices_array = np.array([(1, 4), (2, 5), (3, 6)], dtype=INDICES_DTYPE)
+    with pytest.raises(ShapeMismatch):
+        dp.add_persistent_array(
+            matrix="sa_matrix",
+            data_array=data_array,
+            name="sa-data-vector",
+            indices_array=indices_array,
+        )
+
+
+def test_add_persistent_array_data_shapemismatch_nrows():
+    dp = create_datapackage()
+    data_array = np.arange(12).reshape(4, 3)
+    indices_array = np.array([(1, 4), (2, 5), (3, 6)], dtype=INDICES_DTYPE)
+    with pytest.raises(ShapeMismatch):
+        dp.add_persistent_array(
+            matrix="sa_matrix",
+            data_array=data_array,
+            name="sa-data-vector",
+            indices_array=indices_array,
+        )
+
+
+def test_add_persistent_array_flip_dtype():
+    dp = create_datapackage()
+    data_array = np.arange(12).reshape(3, 4)
+    flip_array = np.array([0, 1, 0])
+    indices_array = np.array([(1, 4), (2, 5), (3, 6)], dtype=INDICES_DTYPE)
+    with pytest.raises(WrongDatatype):
+        dp.add_persistent_array(
+            matrix="sa_matrix",
+            data_array=data_array,
+            name="sa-data-vector",
+            flip_array=flip_array,
+            indices_array=indices_array,
+        )
+
+
+def test_add_persistent_array_flip_shapemistmatch():
+    dp = create_datapackage()
+    data_array = np.arange(12).reshape(3, 4)
+    flip_array = np.array([0, 1, 0, 1], dtype=bool)
+    indices_array = np.array([(1, 4), (2, 5), (3, 6)], dtype=ShapeMismatch)
+    with pytest.raises(ShapeMismatch):
+        dp.add_persistent_array(
+            matrix="sa_matrix",
+            data_array=data_array,
+            name="sa-data-vector",
+            flip_array=flip_array,
+            indices_array=indices_array,
+        )
+
+
+def test_add_dynamic_array_flip_dtype():
+    dp = create_datapackage()
+    flip_array = np.array([0, 1, 0])
+    indices_array = np.array([(1, 4), (2, 5), (3, 6)], dtype=INDICES_DTYPE)
+    with pytest.raises(WrongDatatype):
+        dp.add_dynamic_array(
+            matrix="sa_matrix",
+            interface=Dummy(),
+            name="sa-data-vector",
+            flip_array=flip_array,
+            indices_array=indices_array,
+        )
+
+
+def test_add_dynamic_array_flip_shapemistmatch():
+    dp = create_datapackage()
+    flip_array = np.array([0, 1, 0, 1], dtype=bool)
+    indices_array = np.array([(1, 4), (2, 5), (3, 6)], dtype=ShapeMismatch)
+    with pytest.raises(ShapeMismatch):
+        dp.add_dynamic_array(
+            matrix="sa_matrix",
+            interface=Dummy(),
+            name="sa-data-vector",
+            flip_array=flip_array,
+            indices_array=indices_array,
+        )
+
+
+def test_add_dynamic_vector_flip_dtype():
+    dp = create_datapackage()
+    flip_array = np.array([0, 1, 0])
+    indices_array = np.array([(1, 4), (2, 5), (3, 6)], dtype=INDICES_DTYPE)
+    with pytest.raises(WrongDatatype):
+        dp.add_dynamic_vector(
+            matrix="sa_matrix",
+            interface=Dummy(),
+            name="sa-data-vector",
+            flip_array=flip_array,
+            indices_array=indices_array,
+        )
+
+
+def test_add_dynamic_vector_flip_shapemistmatch():
+    dp = create_datapackage()
+    flip_array = np.array([0, 1, 0, 1], dtype=bool)
+    indices_array = np.array([(1, 4), (2, 5), (3, 6)], dtype=ShapeMismatch)
+    with pytest.raises(ShapeMismatch):
+        dp.add_dynamic_vector(
+            matrix="sa_matrix",
+            interface=Dummy(),
+            name="sa-data-vector",
+            flip_array=flip_array,
+            indices_array=indices_array,
+        )
